bd1f6e0fd9877a38d26b9655385af9ce3c9ea3fc
Eric Brown <eric.brown8787@gmail.com>
2022-01-02 15:17:05 -0500

    adding change color on mouse hover functionality

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-const etchContainer = document.querySelector("etchContainer");
+const etchContainer = document.getElementById("etchContainer");
+
+// const etchBoxContent = document.createTextNode("meow");
+
+function populateGrid(columns, rows) {
+  for (let i = 0; i < columns * rows; i++) {
+    let gridBox = document.createElement("div");
+    gridBox.classList.add("etchPixel");
+    etchContainer.appendChild(gridBox);
+  }
+  changeColor();
+}
+
+function changeColor() {
+  let gridContents = document.querySelectorAll(".etchPixel");
+  gridContents.forEach((item) => {
+    item.addEventListener("mouseenter", () => {
+      item.style.backgroundColor = "#000000";
+    });
+  });
+}
+
+populateGrid(16, 16);
+// etchContainer.innerHTML += etchBoxContent;
