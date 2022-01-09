37353f9db35112be819660ad93b7716d4ea98617
Eric Brown <eric.brown8787@gmail.com>
2022-01-08 19:02:38 -0500

    Added input field for user defined grid dimensions

--- app.js
+++ app.js
@@ -1,31 +1,75 @@
-const etchContainer = document.getElementById("etchContainer");
-const root = document.querySelector(":root");
+// const gridContainer = document.getElementById("gridContainer");
+// const root = document.querySelector(":root");
 
 // const etchBoxContent = document.createTextNode("meow");
 
 function populateGrid(squaresPerSide) {
-  let boxHeight = etchContainer.offsetHeight / squaresPerSide;
-  let boxWidth = etchContainer.offsetWidth / squaresPerSide;
+  const gridContainer = document.getElementById("gridContainer");
+  const gridContents = document.createDocumentFragment();
+  const root = document.querySelector(":root");
+  let boxHeight = gridContainer.offsetHeight / squaresPerSide;
+  let boxWidth = gridContainer.offsetWidth / squaresPerSide;
+
+  //Allocates space in grid based on input value
   root.style.setProperty("--grid-columns", `${squaresPerSide}`);
   root.style.setProperty("--grid-rows", `${squaresPerSide}`);
+
+  //Clears previous grid content, if any
+  gridContainer.innerHTML = "";
+
   for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
     let gridBox = document.createElement("div");
-    gridBox.style.cssText = `height: ${boxHeight}px; width: ${boxWidth}px`;
+    gridBox.style.cssText = `height: ${boxHeight}px; 
+      width: ${boxWidth}px;
+      outline: 1px solid black;
+      margin: 0;
+      padding: 0;
+      background-color: #ffffff;`;
     gridBox.classList.add("etchPixel");
-    etchContainer.appendChild(gridBox);
+    gridBox.textContent = " ";
+    gridContents.appendChild(gridBox);
   }
 
-  changeColor();
+  gridContainer.appendChild(gridContents);
 }
 
 function changeColor() {
-  let gridContents = document.querySelectorAll(".etchPixel");
-  gridContents.forEach((item) => {
-    item.addEventListener("mouseenter", () => {
-      item.style.backgroundColor = "#000000";
+  const etchPixels = document.querySelectorAll(".etchPixel");
+  etchPixels.forEach((pixel) => {
+    pixel.addEventListener("mouseenter", () => {
+      pixel.style.backgroundColor = "#000000";
     });
   });
 }
 
-populateGrid(64);
-// etchContainer.innerHTML += etchBoxContent;
+function generateNewGrid() {
+  populateGrid(gridDimensions);
+  changeColor();
+}
+
+const generateGridButton = document.getElementById("generateGridButton");
+
+const gridDimensionsInput = document.getElementById("gridDimensions");
+
+let gridDimensions = gridDimensionsInput.valueAsNumber;
+
+// gridDimensionsInput.addEventListener("input", (inputEvent) => {
+//   const charCode = inputEvent.charCode;
+//   if (charCode < 48 || charCode > 57) {
+//     inputEvent.preventDefault();
+//     alert("Invalid!");
+//     console.log("this worked");
+//   }
+// });
+
+gridDimensionsInput.addEventListener("input", () => {
+  gridDimensions = gridDimensionsInput.valueAsNumber;
+  const dimensionsMultiplier = document.getElementById("dimensionsMultiplier");
+  dimensionsMultiplier.innerHTML = ` x ${gridDimensions}`;
+});
+
+generateGridButton.addEventListener("click", () => {
+  generateNewGrid();
+});
+
+generateNewGrid(gridDimensions);
